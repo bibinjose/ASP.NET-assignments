cc8ba7bd677b4f762f72097487da0c26f8cf09d4
bibinjose <bibinjose987@gmail.com>
2018-03-02 19:36:36 -0500

    chapter 8
    ASP.NET CORE chapter 8

--- SportsStore Core/SportsStore/SportsStore/Startup.cs
+++ SportsStore Core/SportsStore/SportsStore/Startup.cs
@@ -15,7 +15,15 @@ namespace SportsStore
     {
         public Startup(IConfiguration configuration) => Configuration = configuration;
         public IConfiguration Configuration { get; }
-        public void ConfigureServices(IServiceCollection services) { services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"])); services.AddTransient<IProductRepository, EFProductRepository>(); services.AddMvc(); }
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) { app.UseDeveloperExceptionPage(); app.UseStatusCodePages(); app.UseStaticFiles(); app.UseMvc(routes => { routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}"); }); SeedData.EnsurePopulated(app); }
+        public void ConfigureServices(IServiceCollection services) { services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>(); services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage(); app.UseStatusCodePages(); app.UseStaticFiles(); app.UseMvc(routes => {
+                routes.MapRoute(name: "pagination", template: "Products/Page{productPage}", defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
+            }); SeedData.EnsurePopulated(app);
+        }
     }
 }
